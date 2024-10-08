3b8585b6ea3b99cdb1beaa667a60b94d8f2d5f46
Hamster.cvv <Bhartidabi.059@gmail.com>
2024-10-08 14:17:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Firebase or any login functionality can be added here later
+
+function login() {
+  alert('Login function goes here!');
+}
+
+function logout() {
+  alert('Logout function goes here!');
+}
